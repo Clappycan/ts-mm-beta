1cfc1ff6538870e1cc0d94923156cefa0638fb7f
Clappycan <140260238+Clappycan@users.noreply.github.com>
2023-12-13 12:13:36 -0500

    changes to files

--- tsmm/Main.java
+++ tsmm/Main.java
@@ -3,9 +3,15 @@ import java.util.logging.*;
 
 public class Main {
     public static void main(String[] args) {
-        Piece knight = new Piece("a", 1, "N");
+        Piece[] wPieces;
+        Piece[] bPieces;
         Logger logger = Logger.getLogger(Main.class.getName());
         logger.setLevel(Level.INFO);
-        logger.info(knight.getPosition());
+        logger.info("");
+    }
+
+    public static void boardSetup() {
+        String[] xValues = {"a", "b", "c", "d", "e", "f", "g", "h"};
+        String[] idenValues = {"R", "N", "B", "Q", "K"};
     }
 }
